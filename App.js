3bfb2d41875e2e716d2581dc11ebf4def61b7e6d
Nirlena <102743997+Nirlena@users.noreply.github.com>
2023-02-06 11:35:07 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Platform, StatusBar, TouchableOpacity, Image } from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb'
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: '',
+      chunks: [],
+    }
+  }
+  render() {
+    return (
+        <SafeAreaView style={styles.droidSafeArea}>
+        <Header
+          backgroundColor='#9c47c2'
+          centerComponent={{ text: 'Fonema', style: { color: '#000', fontSize: 20 } }} />
+        <Image style={styles.imageIcon} source={require('./assets/boca.png')} />
+        <TextInput
+          onChangeText={text => { this.setState({ text: text }) }}
+          value={this.state.text}
+          style={styles.inputBox}/>
+        
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={()=>{this.setState({ chunks: db[this.state.text].chunks })}}>
+          <Text style={styles.buttonText}>
+            Go
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.displayText}>
+          {this.state.displayText}
+        </Text>
+        <View>
+          {this.state.chunks.map((item)=>{
+            return(
+              <TouchableOpacity style={styles.chunkButton}>
+                <Text style={styles.displayText}>
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            )
+          })}
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  },
+  inputBox: {
+    marginTop: 200,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    fontWeight: 'bold'
+  },
+  goButton: {
+    width: 65,
+    height: 65,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 25,
+    borderWidth: 4,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#cdcdcd'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  displayText: {
+    alignItems: 'center',
+    fontSize: 30
+    },
+  imageIcon: {
+    width: 150,
+    height: 100,
+    marginLeft: 100,
+    },
+    chunkButton: {
+    backgroundColor: 'red',
+    width: '60%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 10
+    }
+});
